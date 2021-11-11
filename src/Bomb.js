19243e3b776f1a3743a20b12ea87a600ddebcaf1
sbigelo <sean.bigelow1@gmail.com>
2021-11-11 14:17:52 -0600

    commit tree f62f4167b17f919a9105fe08350c80605d88c603

--- src/Bomb.js
+++ src/Bomb.js
@@ -2,24 +2,24 @@ import React from 'react'
 
 class Bomb extends React.Component {
 
-    constructor(props) {
-        super()
-        this.state = {
-            secondsLeft: props.initialCount
-        }
+constructor(props) {
+    super()
+    this.state = {
+        secondsLeft: props.initialCount
     }
+}
 
-
-
-
-    render() {
-        const boom = this.state.secondsLeft === 0 ? `Boom!` : `${this.state.secondsLeft} seconds left before I go boom!`
+render() {
+    const boom = this.state.secondsLeft === 0 ? <h2>Boom!</h2> : <h2>{this.state.secondsLeft} seconds left before I go boom!</h2> 
    
-        return(
-        <div>{boom}</div>
+    return(
+        <div>
+            {/* <h2>{this.state.secondsLeft} seconds left before I go boom!</h2> */}
+            {boom}
             
-        )
-    }
+        </div>
+    )
+}
 
 }
 
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -3,19 +3,19 @@ import React from 'react'
 class ImageSlider extends React.Component {
 
     constructor() {
-      super()
-      this.state = {
-          currentSlideIndex: 0
-      }
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
     }
-    
 
     render() {
-        return(
-            `I am on slide ${this.state.currentSlideIndex}`
-        )
-    }
-
+    return(
+        <div>
+            <h1>I am on slide {this.state.currentSlideIndex}</h1>
+        </div>
+    )
+}
 }
 
 export default ImageSlider
